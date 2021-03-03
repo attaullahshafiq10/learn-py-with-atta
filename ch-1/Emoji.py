03131a154c24a54dbeb38651ce2215823b44d12f
Hafiz Muhammad Attaullah <attaullahshafiq10@gmail.com>
2021-03-03 10:51:41 +0500

    Add files via upload

---
+++ ch-1/Emoji.py
@@ -0,0 +1,6 @@
+# https://unicode.org/emoji/charts/full-emoji-list.html
+
+print("\U0001f602") # In U+1f603, you must replce "000" from "+". and just add backslash at starting point
+print("\U0001f604")
+print("\U0001f603")
+# find the more emojis from unicode
